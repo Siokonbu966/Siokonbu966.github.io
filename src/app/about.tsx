3865d40706cd966e9e6c35197eafe67c48b937f1
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2025-12-10 23:12:49 +0900

    feat: 背景を画面いっぱいに表示させる

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -7,11 +7,13 @@ import WorksSection from "@/components/sections/WorksSection";
 export default function AboutPage() {
   return (
     <>
-      <div className="bg-blue-900">
-        <ProfileSection />
-        <SkillsSection />
-        <WorksSection />
-        <HistorySection />
+      <div className="bg-blue-900 text-white">
+        <div className="mx-auto max-w-5xl min-h-screen sm:px-6 lg:px-8">
+          <ProfileSection />
+          <SkillsSection />
+          <WorksSection />
+          <HistorySection />
+        </div>
         <Footer />
       </div>
     </>
