24439f7385831d1aacd857e809648990362ce509
Siokonbu966 <167207736+Siokonbu966@users.noreply.github.com>
2026-03-02 22:53:48 +0900

    fix: 削除忘れののsectionを削除

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -2,7 +2,6 @@ import Footer from "@/components/Footer";
 import HistorySection from "@/components/sections/HistorySection";
 import ProfileSection from "@/components/sections/ProfileSection"
 import SkillsSection from "@/components/sections/SkillsSection"
-import WorksSection from "@/components/sections/WorksSection";
 
 export default function AboutPage() {
   return (
